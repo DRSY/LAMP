8d9605c2d4cac1145839786fe9f4c8393bc7bf8d
roy <rsy0702@163.com>
2020-11-01 16:25:35 +0800

    Collator

--- config.py
+++ config.py
@@ -1,21 +1,27 @@
 '''
 Author: roy
 Date: 2020-11-01 11:16:54
-LastEditTime: 2020-11-01 14:12:27
+LastEditTime: 2020-11-01 15:50:28
 LastEditors: Please set LastEditors
 Description: In User Settings Edit
 FilePath: /LAMA/config.py
 '''
 import argparse
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(filename)s [line:%(lineno)d] %(levelname)s %(message)s', datefmt='%a, %d %b %Y %H:%M:%S')
+logger = logging.getLogger(__name__)
 
 conceptNet_path = "./data/ConceptNet/test.jsonl"
 place_of_birth_path = "./data/Google_RE/place_of_birth_test.jsonl"
 place_of_death_path = "./data/Google_RE/place_of_death_test.jsonl"
 
+
 def get_args():
     parser = argparse.ArgumentParser(
         "Probing knwoledge in pretrained language model using self-masking")
-    parser.add_argument('--model_name', type=str, default='bert-base-uncased')
+    parser.add_argument('--model_name', type=str, default='bert-base-uncased', help="name of pretrained language model")
+    parser.add_argument('--max_length', type=int, default=20, help="max length of input cloze question")
     args = parser.parse_args()
     return args
-
--- data.py
+++ data.py
@@ -1,26 +1,46 @@
 '''
 Author: roy
 Date: 2020-11-01 11:08:20
-LastEditTime: 2020-11-01 14:13:29
+LastEditTime: 2020-11-01 16:25:20
 LastEditors: Please set LastEditors
 Description: In User Settings Edit
 FilePath: /LAMA/data.py
 '''
+from utils import LAMA
 import torch
+import jsonlines
 from torch.utils.data import DataLoader, Dataset, RandomSampler
 from typing import *
+from transformers import AutoTokenizer
 
-from config import (conceptNet_path, place_of_birth_path, place_of_death_path)
+from config import (conceptNet_path, place_of_birth_path,
+                    place_of_death_path, logger, get_args)
 
 
 class LAMADataset(Dataset):
     """
+    Customized Dataset for loading LAMA dataset
     """
 
-    def __init__(self, paths: List[str]) -> None:
+    def __init__(self, path: str) -> None:
         super().__init__()
-        self.paths = paths
+        self.path = path
         self.datas = []
+        self.read_data(self.path)
+
+    def read_data(self, path: str):
+        assert path.endswith('jsonl'), "not a jsonline file"
+        logger.info("start reading file {}".format(path))
+        with open(path, mode='r', encoding='utf-8') as f:
+            for instance in jsonlines.Reader(f):
+                masked_sentences = instance['masked_sentences']
+                relation = instance['pred']
+                obj_label = instance['obj_label']
+                if '[MASK]' not in masked_sentences[0]:
+                    continue
+                self.datas.append((masked_sentences[0], obj_label, relation))
+        logger.info("finish reading file {}, get {} instances".format(
+            path, len(self.datas)))
 
     def __len__(self):
         return len(self.datas)
@@ -31,10 +51,78 @@ class LAMADataset(Dataset):
 
 class Collator(object):
     """
+    Collator class for gathering samples within a mini-batch
     """
 
-    def __init__(self) -> None:
-        pass
+    relation2id = {
+        'IsA': 0,
+    }
+
+    def __init__(self, model_name: str, max_length: int) -> None:
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.mask_token = self.tokenizer.mask_token
+        self.mask_token_id = self.tokenizer.mask_token_id
+        self.max_length = max_length
+
+    def get_label(self, input_ids: List[int], obj_label: str):
+        mask_token_index = input_ids.index(self.mask_token_id)
+        labels = input_ids.clone()
+        labels.fill_(-100)
+        labels[mask_token_index] = self.tokenizer.convert_tokens_to_ids([obj_label])[
+            0]
+        return labels
 
     def __call__(self, data_batch: List):
-        pass
+        def merge_batch(data_batch: List):
+            masked_sentences = [data[0] for data in data_batch]
+            obj_labels = [data[1] for data in data_batch]
+            relations = [data[-1] for data in data_batch]
+            return masked_sentences, obj_labels, relations
+
+        if self.tokenizer.mask_token != '[MASK]':
+            for data in data_batch:
+                data[0] = data[0].replace('[MASK]', self.mask_token)
+        bs = len(data_batch)
+        masked_sentences, obj_labels, relations = merge_batch(
+            data_batch=data_batch)
+        relations_in_batch = set()
+        tmp_batch_dict = dict()
+        for i in range(bs):
+            relation_id = self.relation2id.get(relations[i])
+            relations_in_batch.add(relation_id)
+            if not relation_id in tmp_batch_dict:
+                tmp_batch_dict[relation_id] = {
+                    'masked_sentences': [], 'obj_labels': []}
+            else:
+                tmp_batch_dict[relation_id]['masked_sentences'].append(
+                    masked_sentences[i])
+                tmp_batch_dict[relation_id]['obj_labels'].append(obj_labels[i])
+        relations_in_batch = list(relations_in_batch)
+        num_relations = len(relations_in_batch)
+        input_dict_list = []
+        labels_list = []
+        for relation_id in relations_in_batch:
+            batch_input_dict = self.tokenizer(tmp_batch_dict[relation_id]['masked_sentences'], max_length=self.max_length,
+                                              padding='max_length', truncation=True, return_tensors='pt', return_attention_mask=True)
+            batch_input_ids = batch_input_dict['input_ids'].tolist()
+            labels = []
+            for i in range(len(tmp_batch_dict[relation_id]['masked_sentences'])):
+                label = self.get_label(
+                    batch_input_ids[i], tmp_batch_dict[relation_id]['obj_labels'][i])
+                labels.append(label)
+            labels = torch.tensor(labels).type(
+                batch_input_dict['input_ids'].dtype)
+            tmp_batch_dict[relation_id]['labels'] = labels
+            tmp_batch_dict[relation_id]['input_dict'] = batch_input_dict
+            input_dict_list.append(batch_input_dict)
+            labels_list.append(labels)
+        return input_dict_list, labels_list, relations_in_batch
+
+
+def test():
+    toy_dataset = LAMADataset(conceptNet_path)
+    print(len(toy_dataset))
+
+
+if __name__ == "__main__":
+    test()

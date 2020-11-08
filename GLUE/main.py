9cb72798db98570707f5eff273a24621491b58f4
roy <rsy0702@163.com>
2020-11-08 19:34:33 +0800

    main

--- GLUE/main.py
+++ GLUE/main.py
@@ -1,7 +1,7 @@
 '''
 Author: roy
 Date: 2020-11-07 15:49:03
-LastEditTime: 2020-11-08 14:48:43
+LastEditTime: 2020-11-08 19:33:50
 LastEditors: Please set LastEditors
 Description: In User Settings Edit
 FilePath: /LAMA/GLUE/main.py
@@ -10,11 +10,17 @@ import sys
 import os
 sys.path.append(os.getcwd())
 
+import logging
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(filename)s [line:%(lineno)d] %(levelname)s:  %(message)s', datefmt='%a, %d %b %Y %H:%M:%S')
+logger = logging.getLogger(__name__)
+
 from GLUE.glue_model import *
 from GLUE.glue_datamodule import *
+from torch.nn.utils import prune
 
 
-def load_masks(pl_model: GLUETransformer, model_name, bli, tli):
+def apply_masks(pl_model: GLUETransformer, model_name, bli, tli, masks):
     backbone = pl_model.model
     model_type = model_name.split('-')[0]
     assert hasattr(backbone, model_type), f"LM does not have {model_type}"
@@ -26,8 +32,46 @@ def load_masks(pl_model: GLUETransformer, model_name, bli, tli):
         layers = backbone.bert.encoder.layer
 
     # load pre-trained masks
-    for ii in range(bli, tli+1):
-        pass
+    parameters_tobe_pruned = []
+    for i in range(bli, tli+1):
+        try:
+            parameters_tobe_pruned.append(
+                (layers[i].attention.self.query, 'weight'))
+            parameters_tobe_pruned.append(
+                (layers[i].attention.self.key, 'weight'))
+            parameters_tobe_pruned.append(
+                (layers[i].attention.self.value, 'weight'))
+            parameters_tobe_pruned.append(
+                (layers[i].attention.output.dense, 'weight'))
+            parameters_tobe_pruned.append(
+                (layers[i].intermediate.dense, 'weight'))
+            parameters_tobe_pruned.append(
+                (layers[i].output.dense, 'weight'))
+        except Exception:
+            parameters_tobe_pruned.append(
+                (layers[i].attention.q_lin, 'weight')
+            )
+            parameters_tobe_pruned.append(
+                (layers[i].attention.k_lin, 'weight')
+            )
+            parameters_tobe_pruned.append(
+                (layers[i].attention.v_lin, 'weight')
+            )
+            parameters_tobe_pruned.append(
+                (layers[i].attention.out_lin, 'weight')
+            )
+            parameters_tobe_pruned.append(
+                (layers[i].ffn.lin1, 'weight')
+            )
+            parameters_tobe_pruned.append(
+                (layers[i].ffn.lin2, 'weight')
+            )
+    assert len(masks) == len(parameters_tobe_pruned), f"{parameters_tobe_pruned} != {len(masks)}"
+    for mask, (module, name) in zip(masks, parameters_tobe_pruned):
+        prune.custom_from_mask(module, name, mask)
+    logger.info("Pre-computed mask applied to {}".format(model_name))
+    
+    
 
 
 def parse_args():
